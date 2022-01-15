4fad5516aff54b489a008489ffa88731dd9d7348
bnuri <bnurik104@gmail.com>
2022-01-15 19:21:14 +0900

    FIX: canvas 이미지 로딩되지 않는 오류 수정
    - js 파일 이름 변경(event.js -> myEvent.js)
    - window.onload 이벤트로 변경

--- mysite/views.py
+++ mysite/views.py
@@ -6,3 +6,8 @@ def index(request):
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
     return render(request, 'mysite/index.html', context)
+
+def detail(request, content_id):
+    content_list = MainContent.objects.get(id=content_id)
+    context = { 'content_list': content_list}
+    return render(request, 'mysite/content_detail.html', context)
