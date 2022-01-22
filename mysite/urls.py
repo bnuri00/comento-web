612e94c3f3ff9a05441ee0fc9389830519eb695d
bnuri <bnurik104@gmail.com>
2022-01-23 01:54:26 +0900

    FEAT: 댓글기능 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+app_name = 'mysite'
 
 urlpatterns = [
     path('', views.index, name='new'),
     path('prod/', views.prodAll, name='prodAll'),
     path('<str:cate_name>/', views.prodList, name='prodList'),
     path('detail/<int:product_id>/', views.detail, name='detail'),  # ???
+    path('comment/create/<int:product_id>/', views.comment_create, name='comment_create'),
+
+    path('comment/update/<int:comment_id>/', views.comment_update, name='comment_update'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Product, Category
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product, Category, Comment
+from .forms import CommentForm
 
 
 def index(request):
@@ -7,10 +10,12 @@ def index(request):
     context = {'content_list': content_list}
     return render(request, 'mysite/index.html', context)
 
+
 def prodAll(request):
     product_list = Product.objects.order_by('-pub_date')
     product = {'product_list': product_list}
     return render(request, 'mysite/product_list.html', product)
+
 
 # Category Product list
 def prodList(request, cate_name):
@@ -19,7 +24,54 @@ def prodList(request, cate_name):
     content = {'product_list': product_list, 'category_list': category_list}
     return render(request, 'mysite/product_list.html', content)
 
+
 def detail(request, product_id):
     product_list = get_object_or_404(Product, pk=product_id)
     product = {'product_list': product_list}
     return render(request, 'mysite/content_detail.html', product)
+
+
+@login_required(login_url='accounts:login')
+def comment_create(request, product_id):
+    product_list = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.content_list = product_list
+            comment.author = request.user
+            comment.save()
+            return redirect('mysite:detail', product_id=product_list.id)
+        else:
+            form = CommentForm()
+
+    context = {'content_list': product_list, 'form': form}
+    return render(request, 'mysite/content_detail.html', context)
+
+
+@login_required(login_url='accounts:login')
+def comment_update(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        raise PermissionDenied
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('mysite:detail', product_id=comment.content_list.id)
+    else:
+        form = CommentForm(instance=comment)
+    context = {'comment': comment, 'form': form}
+    return render(request, 'mysite/comment_form.html', context)
+
+
+@login_required(login_url='accounts:login')
+def comment_delete(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        raise PermissionDenied
+    else:
+        comment.delete()
+    return redirect('mysite:detail', product_id=comment.content_list.id)
